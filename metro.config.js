57823e410d659ce1d3c2e7efce9c75e103ecb7a1
Gabriel Webb <gabrielwebb90@gmail.com>
2026-01-03 20:22:31 -0500

    [Checkpoint] so not this?

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add polyfills for Node.js modules
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    ...config.resolver?.extraNodeModules,
+    buffer: require.resolve('buffer'),
+  },
+};
+
 module.exports = config;
